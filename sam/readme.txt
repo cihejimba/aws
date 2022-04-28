8d4ff5e68c5c878468c6dfbc2ff6eaeb25fe9576
Thai Dang <thai.dang.fr@gmail.com>
2022-04-28 17:40:22 +0000

    working

--- sam/readme.txt
+++ sam/readme.txt
@@ -1,4 +1,26 @@
-https://seanjziegler.com/how-to-build-an-api-with-aws-lambda-and-api-gateway-using-aws-sam/
+# https://seanjziegler.com/how-to-build-an-api-with-aws-lambda-and-api-gateway-using-aws-sam/
+# https://www.honeybadger.io/blog/ruby-aws-lambda/
+# https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-policy-templates.html
 
+first time
+==========
 sam init
+sam local generate-event apigateway aws-proxy > events/translate.json
+sam local invoke -e events/translate.json
 sam deploy --guided
+
+marche pas ?
+============
+sam local invoke -e events/translate.json
+
+  "errorMessage": "The security token included in the request is invalid",
+  "errorType": "Function<Aws::DynamoDB::Errors::UnrecognizedClientException>",
+
+
+build et deploy
+===============
+sam build && sam deploy
+
+test 
+====
+curl -X POST https://o0d6eai1rj.execute-api.us-east-1.amazonaws.com/Prod/translate?text=salut
--- sam/samapp/translate/app.rb
+++ sam/samapp/translate/app.rb
@@ -1,5 +1,14 @@
-# require 'httparty'
 require 'json'
+require 'securerandom'
+require 'aws-record'
+
+class TextsTable
+  include Aws::Record
+  set_table_name ENV['TEXTS_TABLE']
+  string_attr :id, hash_key: true
+  string_attr :text
+  string_attr :expired_at
+end
 
 def lambda_handler(event:, context:)
   # Sample pure Lambda function
@@ -21,18 +30,22 @@ def lambda_handler(event:, context:)
   #     # api-gateway-simple-proxy-for-lambda-output-format
   #     Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
 
-  # begin
-  #   response = HTTParty.get('http://checkip.amazonaws.com/')
-  # rescue HTTParty::Error => error
-  #   puts error.inspect
-  #   raise error
-  # end
+  text = event['queryStringParameters']['text']
+  uuid = SecureRandom.uuid
+  expired_at = (Time.now + 60*60*24).to_i
 
+  item = TextsTable.new(id: uuid, text: text, expired_at: expired_at)
+  item.save!
+  item.to_h
+  
   {
     statusCode: 200,
     body: {
-      message: "Hello World!",
-      # location: response.body
+      text: text,
+      uuid: uuid,
+      expired_at: expired_at
     }.to_json
   }
+  
 end
+
