14aad00fb513668b1af97b3d0222636fdf32b968
Thai Dang <thai.dang.fr@gmail.com>
2022-05-01 21:34:15 +0000

    fix

--- rails/delete.sh
+++ rails/delete.sh
@@ -4,5 +4,8 @@ export AWS_PAGER=""
 aws cloudformation delete-stack --stack-name rails-service
 aws cloudformation wait stack-delete-complete --stack-name rails-service
 
+aws cloudformation delete-stack --stack-name rails-database
+aws cloudformation wait stack-delete-complete --stack-name rails-database
+
 aws cloudformation delete-stack --stack-name rails-network
 aws cloudformation wait stack-delete-complete --stack-name rails-network
