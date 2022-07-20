05330aa010a3e6b1ff8f364ec68d92c1797fc599
Thai DANG <thai.dang.fr@gmail.com>
2022-07-20 18:16:51 +0000

    unstable

--- terraform/eb/eb.tf
+++ terraform/eb/eb.tf
@@ -11,10 +11,29 @@ resource "aws_elastic_beanstalk_application_version" "default" {
   key         = aws_s3_object.default.id
 }
 
+# https://automateinfra.com/2021/03/24/how-to-launch-aws-elastic-beanstalk-using-terraform/
 #aws elasticbeanstalk list-available-solution-stacks
 #64bit Amazon Linux 2 v3.3.14 running Python 3.8
 resource "aws_elastic_beanstalk_environment" "tfenvtest" {
   name                = "tf-test-name"
   application         = aws_elastic_beanstalk_application.tftest.name
   solution_stack_name = "64bit Amazon Linux 2 v3.3.14 running Python 3.8"
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = "vpc-0e75a6c12ef09bc6c"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = "subnet-06d429be7739347da,subnet-038d83167aa1aa1f5"
+  }
+
+    setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     =  "aws-elasticbeanstalk-ec2-role"
+  }
 }
